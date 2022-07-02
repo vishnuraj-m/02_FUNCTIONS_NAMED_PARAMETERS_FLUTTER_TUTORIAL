42db6581a4354cfbe2bf6926f5218d94f0a02a08
vishnuacappella <acappella.studiov@gmail.com>
2022-07-02 21:18:26 +0530

    function with parameter with return value complete

--- bin/functions.dart
+++ bin/functions.dart
@@ -1,8 +1,23 @@
 void main() {
   sum();
+
+  // *function with parameter with return value
+  sumParams(1, 2);
+  var sum1 = sumReturn(5, 6);
+  print('Sum Return :$sum1');
 }
 
 // !function no parameter no return value
 void sum() {
   print(2 + 3);
+}
+
+// !function with parameter with return value
+
+void sumParams(int a, int b) {
+  print('sum :${a + b}');
+}
+
+int sumReturn(int a, int b) {
+  return a + b;
 }
