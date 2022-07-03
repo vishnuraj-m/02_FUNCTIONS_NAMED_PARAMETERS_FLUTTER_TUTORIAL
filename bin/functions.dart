5841e9b39542d107392a57b30659aec2625af766
vishnuacappella <acappella.studiov@gmail.com>
2022-07-03 10:30:15 +0530

    required parameter completed

--- bin/functions.dart
+++ bin/functions.dart
@@ -5,6 +5,9 @@ void main() {
   sumParams(1, 2);
   var sum1 = sumReturn(5, 6);
   print('Sum Return :$sum1');
+
+  // *Required Parameter
+  sumRequired(firstNumber: 10, secondNumber: 6);
 }
 
 // !function no parameter no return value
@@ -20,4 +23,10 @@ void sumParams(int a, int b) {
 
 int sumReturn(int a, int b) {
   return a + b;
+}
+
+// !Required Parameter
+
+void sumRequired({required int firstNumber, required int secondNumber}) {
+  print('Sum Required: ${firstNumber + secondNumber}');
 }
